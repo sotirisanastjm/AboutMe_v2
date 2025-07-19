a22b0dcc929ca2fa70c31b4d1db3110f8319f521
Sotirios Anastasiadis <sotiris.anastjm@gmail.com>
2025-07-19 12:33:47 +0300

    Fix Year

--- src/app/testimonial/testimonial.component.ts
+++ src/app/testimonial/testimonial.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-testimonial',
   templateUrl: './testimonial.component.html',
   styleUrls: ['./testimonial.component.scss']
 })
-export class TestimonialComponent {
+export class TestimonialComponent implements OnInit{
+
+  year: any;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.year = new Date().getFullYear();
+  }
 
 }
