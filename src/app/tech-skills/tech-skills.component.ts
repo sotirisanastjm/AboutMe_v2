58d6049324113d4277fd248d1d096803ca27a192
Sotirios Anastasiadis <sotiris.anastjm@gmail.com>
2025-09-28 13:44:31 +0300

    Info Changes and Some ui elements

--- src/app/tech-skills/tech-skills.component.ts
+++ src/app/tech-skills/tech-skills.component.ts
@@ -6,16 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./tech-skills.component.scss']
 })
 export class TechSkillsComponent {
+  
   skillsList = [
     { name: 'Sitecore', svg: '0' },
     { name: 'Angular', svg: '1' },
-    { name: 'React', svg: '2' },
-    { name: 'HTML', svg: '3' },
-    { name: 'CSS', svg: '4' },
-    { name: 'TailWindCSS', svg: '5' },
-    { name: 'JavaScript', svg: '6' },
-    { name: 'TypeScript', svg: '7' },
-    { name: 'Sass', svg: '8' },
+    { name: 'Nextjs', svg: '2' },
+    { name: 'React', svg: '3' },
+    { name: 'HTML', svg: '4' },
+    { name: 'CSS', svg: '5' },
+    { name: 'TailWindCSS', svg: '6' },
+    { name: 'JavaScript', svg: '7' },
+    { name: 'TypeScript', svg: '8' },
+    { name: 'Sass', svg: '9' },
+    { name: '.NET', svg: '10' },
+    { name: 'Sui Move', svg: '11' },
 
   ];
 }
